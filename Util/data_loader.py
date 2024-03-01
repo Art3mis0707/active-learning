06fbc560f8735cbdb743ccd51a91fbac40d55c59
Gayatri <74173683+Art3mis0707@users.noreply.github.com>
2024-03-01 21:49:14 +0530

    Update data_loader.py

--- Util/data_loader.py
+++ Util/data_loader.py
@@ -1,25 +1,44 @@
 import os
+import numpy as np
+from PIL import Image
 import torch
-from torchvision import transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, Dataset, Subset, ConcatDataset
 
-def load_unlabelled_data(data_dir, batch_size=32, img_size=224):
-    transform = transforms.Compose([
-        transforms.Resize((img_size, img_size)),
+def get_transform(img_size=(224, 224)):
+    return transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.CenterCrop(img_size[0]),
         transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    dataset = ImageFolder(os.path.join(data_dir, 'unlabelled'), transform=transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    return dataloader
+class UnlabeledDataset(Dataset):
+    def __init__(self, image_paths, transform=None):
+        self.image_paths = image_paths
+        self.transform = transform
 
-def load_labelled_data(data_dir, batch_size=32, img_size=224):
-    transform = transforms.Compose([
-        transforms.Resize((img_size, img_size)),
-        transforms.ToTensor(),
-    ])
+    def __len__(self):
+        return len(self.image_paths)
 
-    dataset = ImageFolder(os.path.join(data_dir, 'labelled'), transform=transform)
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-    return dataloader
+    def __getitem__(self, idx):
+        image_path = self.image_paths[idx]
+        image = Image.open(image_path).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+        return image, 0  # Return a dummy label
+
+def load_initial_subset(labelled_data_dir, batch_size=32, initial_sample_size=100):
+    transform = get_transform()
+    all_dataset = datasets.ImageFolder(root=labelled_data_dir, transform=transform)
+    indices = np.random.choice(len(all_dataset), initial_sample_size, replace=False)
+    initial_dataset = Subset(all_dataset, indices)
+    return DataLoader(initial_dataset, batch_size=batch_size, shuffle=True), all_dataset
+
+def create_unlabeled_loader(unlabeled_data_dir, batch_size=32, img_size=(224, 224)):
+    image_paths = [os.path.join(root, name)
+                   for root, dirs, files in os.walk(unlabeled_data_dir)
+                   for name in files
+                   if name.endswith((".png", ".jpg", ".jpeg"))]
+    dataset = UnlabeledDataset(image_paths=image_paths, transform=get_transform(img_size))
+    return DataLoader(dataset, batch_size=batch_size, shuffle=True)
